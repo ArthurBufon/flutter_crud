c78870b6881b6fa7a87a4691ee8d164f1a034ab5
Arthur <arthurbufon2052@gmail.com>
2023-05-31 19:43:24 -0300

    first commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = 
+{
+  '1': User
+  (
+    id: '1',
+    name: 'Arthur',
+    email: 'arthur@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_1280.png',
+  ),
+  '2': User
+  (
+    id: '2',
+    name: 'Joao',
+    email: 'joao@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_1280.png',
+  ),
+  '3': User
+  (
+    id: '3',
+    name: 'Marquinhos',
+    email: 'https://cdn.pixabay.com/photo/2020/10/11/19/51/cat-5646889_1280.jpg',
+    avatarUrl: '1',
+  ),
+};
